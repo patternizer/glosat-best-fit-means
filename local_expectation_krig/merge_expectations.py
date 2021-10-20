5e3eab2191d663bbb718975e1dd288d13f8badd1
patternizer <michael.a.taylor@uea.ac.uk>
2021-10-20 09:25:24 +0100

    Added header

--- local_expectation_krig/merge_expectations.py
+++ local_expectation_krig/merge_expectations.py
@@ -1,3 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#------------------------------------------------------------------------------
+# PROGRAM: merge_expectations.py
+#------------------------------------------------------------------------------
+# Verion 0.1
+# 8 August, 2021
+# Michael Taylor
+# https://patternizer.github.io
+# patternizer AT gmail DOT com
+# michael DOT a DOT taylor AT uea DOT ac DOT uk
+#------------------------------------------------------------------------------
+
 import os
 import glob
 import pandas as pd
